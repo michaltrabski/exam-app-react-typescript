8b9b6bc399c73f6ebe130d9e99f3b193679d61ec
Michal Trabski <instruktormichaltrabski@gmail.com>
2020-11-22 15:58:51 +0100

    added login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,20 @@
-import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import { useSelector } from "react-redux";
 import PageWrapper from "../components/PageWrapper";
 import { State } from "../redux/store/store";
+import { Link as ReactRouterLink } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import { label } from "../settings/settings";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 function Login() {
   const x = useSelector((state: State) => state);
@@ -10,10 +22,8 @@ function Login() {
   return (
     <PageWrapper>
       <Grid container spacing={0}>
-        <Grid item xs={12} md={12}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Logowanie
-          </Typography>
+        <Grid item xs={12} md={6}>
+          <SignIn />
         </Grid>
       </Grid>
     </PageWrapper>
@@ -21,3 +31,101 @@ function Login() {
 }
 
 export default Login;
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function SignIn() {
+  const classes = useStyles();
+  const { lang } = useSelector((state: State) => state.uiReducer);
+
+  return (
+    <>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>{label.topMenuLogin.icon}</Avatar>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Logowanie
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            size="large"
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to={label.topMenuRegister.url} component={ReactRouterLink}>
+                Nie masz konta? Zarejestruj się.
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </>
+  );
+}
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import PageWrapper from "../components/PageWrapper";
 import { State } from "../redux/store/store";
-
+import { Link as ReactRouterLink } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
+import { label } from "../settings/settings";
 
 function Register() {
   const x = useSelector((state: State) => state);
@@ -55,12 +53,11 @@ const useStyles = makeStyles((theme) => ({
 
 function SignUp() {
   const classes = useStyles();
+  const { lang } = useSelector((state: State) => state.uiReducer);
 
   return (
     <div className={classes.paper}>
-      <Avatar className={classes.avatar}>
-        <LockOutlinedIcon />
-      </Avatar>
+      <Avatar className={classes.avatar}>{label.topMenuRegister.icon}</Avatar>
       <Typography variant="h4" component="h1" gutterBottom>
         Rejestracja
       </Typography>
@@ -131,8 +128,8 @@ function SignUp() {
         </Button>
         <Grid container justify="flex-end">
           <Grid item>
-            <Link href="#" variant="body2">
-              Already have an account? Sign in
+            <Link to={label.topMenuLogin.url} component={ReactRouterLink}>
+              Masz już konto? Zaloguj się.
             </Link>
           </Grid>
         </Grid>
